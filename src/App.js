f7d4510fe8381367979ba7c033abca9c725a09f4
chiazokam <chiazokamecheta@gmail.com>
2020-10-14 18:47:53 +0100

    task(test): implement gitstart exercise
    - implement gitstart exercise

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 const App = () => {
+
+  const [users, setUsers] = useState([]);
+  const [offlineUsers, setOfflineUsers] = useState([]);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+
   // 20% of the users are online, 80% offline on average.
   // Takes 4-8 secs to complete
   const checkStatus = async (userId) => {
-    return Math.random() > 0.2
+    return Math.random() > 0.8
       ? { status: "offline", id: userId }
       : { status: "online", id: userId };
   };
@@ -32,11 +37,64 @@ const App = () => {
       return the users for which the introductions were successfully sent
   */
 
+  useEffect(() => {
+    fetchUserIds()
+    .then(users => {
+      setUsers(users)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
+
+  let count = useRef(0);
+
+  const sendIntroductionToUsers = async (usersToSendTo) => {
+    try{
+      let onlineUsers = [];
+      let offlineUsers = [];
+
+      for(let i = 0; i < usersToSendTo.length; i++) {
+        const status = await checkStatus(usersToSendTo[i]);
+        if(status.status === 'online') {
+          const sendStatus = await sendIntroduction(usersToSendTo[i]);
+          if(sendStatus){
+            onlineUsers = [...onlineUsers, usersToSendTo[i]];
+          } else {
+            offlineUsers = [...offlineUsers, usersToSendTo[i]];
+          }
+        } else {
+          offlineUsers = [...offlineUsers, usersToSendTo[i]];
+        }
+      }
+      setOnlineUsers((prevState) => [...prevState, ...onlineUsers]);
+      setOfflineUsers((prevState) => [...prevState, ...offlineUsers]);
+    
+      if(count.current < 10 && offlineUsers.length) {
+        count.current = count.current + 1;
+        return (
+          setTimeout(() => {
+            sendIntroductionToUsers(offlineUsers)
+          }, 2000)
+        )
+      }
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    if(users.length > 0) {
+      sendIntroductionToUsers(users);
+    }
+  }, [users])
+
+  
   /*
   Question 2:
-    - Re-try for each user if they are offline or if sendIntroduction is false
-    - Re-try up to maximum 10 times
-    - Each Re-try should have 2 second delay between them
+  - Re-try for each user if they are offline or if sendIntroduction is false
+  - Re-try up to maximum 10 times
+  - Each Re-try should have 2 second delay between them
   */
 
   /*
@@ -52,9 +110,7 @@ const App = () => {
         <div>
           All online users that introductions were sucessfully sent
           <ul>
-            <li>Student 1</li>
-            <li>Student 2</li>
-            <li>Student 3</li>
+            {onlineUsers.map(user => <li>{user}</li>)}
           </ul>
         </div>
       </div>
