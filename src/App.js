459495dcc4ccc0f3642e6041f0a8fd82d555b49d
idawud <ismaildawud96@gmail.com>
2020-10-16 17:16:22 +0000

    send online users introductions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   // 20% of the users are online, 80% offline on average.
   // Takes 4-8 secs to complete
   const checkStatus = async (userId) => {
-    return Math.random() > 0.2
+    return Math.random() > 0.8
       ? { status: "offline", id: userId }
       : { status: "online", id: userId };
   };
@@ -45,18 +45,53 @@ const App = () => {
   - 90% of the introductions are successfully sent in a 2 second window
   - 5000 users in total
   */
+ const checkStatusAndSendIntroductionToUsers = async () => {
+   const usersIds = await fetchUserIds();
+   let successfulUser = [];
+   let onlineUsers = [];
+
+   for(let userId of usersIds){
+     let status = await checkStatus(userId);
+     if (status.status === 'online'){
+      onlineUsers.push(userId);
+     }
+   }
+
+   for(let userId of onlineUsers){ 
+      const isIntroSent = await sendIntroduction(userId);
+      if(isIntroSent){
+        successfulUser.push(userId);
+      }
+   }
+
+  //  console.log({successfulUser});
+  setUserNames(successfulUser);
+ }
+
+ const [userNames, setUserNames] = useState([]);
+ const [isHidden, setIsHidden] = useState(false);
+
+ useEffect(()=> {
+  checkStatusAndSendIntroductionToUsers().catch(err => console.error(err));
+ }, [])
+
+ const showOrHide = () => {
+   setIsHidden(isHidden => !isHidden);
+ }
 
   return (
     <div className="App">
       <div className="App-header">
-        <div>
-          All online users that introductions were sucessfully sent
-          <ul>
-            <li>Student 1</li>
-            <li>Student 2</li>
-            <li>Student 3</li>
-          </ul>
-        </div>
+        <button onClick={showOrHide}>Show/Hide</button>
+        {
+          !isHidden && 
+          <div>
+            All online users that introductions were sucessfully sent
+            <ul>
+              {userNames.map(user => <li key={user}> {user} </li>)}
+            </ul>
+          </div>
+        }
       </div>
     </div>
   );
