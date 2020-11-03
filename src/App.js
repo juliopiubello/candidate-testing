6b648559a2f76f25dd75dafd210653f6385e5e70
Oluwajoba <oluwajobabello@gmail.com>
2020-11-03 02:18:14 +0100

    submit solution / App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const App = () => {
+  const [userWithIntroSuccess, setUserWithIntroSuccess] = useState([]);
+
   const checkStatus = async (userId) => {
     return Math.random() > 0.8
       ? { status: "offline", id: userId }
@@ -21,20 +23,54 @@ const App = () => {
       return the users for which the introductions were successfully sent
   */
 
+  useEffect(() => {
+    async function returnUSerID() {
+      let users = await fetchUserIds();
+
+      // map through the array of user
+      users.map((user, index) => {
+        // check users' status using their id
+        async function getStatus() {
+          let status = await checkStatus(index);
+          // if user is online, send them an introduction
+          if (status.status === "online") {
+            async function sendIntro() {
+              let intro = await sendIntroduction(status.id);
+              // check if sendIntroduction delivered succefully
+              if (intro) {
+                // save succeful user on the state
+                setUserWithIntroSuccess((current) => [...current, user]);
+              }
+            }
+            sendIntro();
+          }
+        }
+        getStatus();
+      });
+    }
+    returnUSerID();
+  }, []);
+
   return (
     <div className="App">
       <div className="App-header">
         <div>
           All online users that introductions were sucessfully sent
-          <ul>
-            <li>Student 1</li>
-            <li>Student 2</li>
-            <li>Student 3</li>
-          </ul>
+          {userWithIntroSuccess ? (
+            <>
+              {userWithIntroSuccess.map((user, index) => (
+                <ul key={index}>
+                  <li>{user} </li>
+                </ul>
+              ))}
+            </>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
