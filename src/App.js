4073e83360b4888bc089bc909de76bf089a79562
juliopiubello <juliopiubellow@gmail.com>
2020-12-17 17:58:46 -0300

    update description

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+  
+  const fetchUserIds = async () => {
+    return ["john.smith", "sara.lee", "jack.ma"];
+  };
+
   const checkStatus = async (userId) => {
     return Math.random() > 0.8
       ? { status: "offline", id: userId }
       : { status: "online", id: userId };
-  };
-
-  const fetchUserIds = async () => {
-    return ["john.smith", "sara.lee", "jack.ma"];
   };
 
   const sendIntroduction = async (userId) => {
@@ -17,8 +18,14 @@ const App = () => {
   };
 
   /*
-    Question 1: Find all online users and send them introductions. 
-      return the users for which the introductions were successfully sent
+    Question 1: 
+    Find all online users and send them introductions. Render the users for which the introductions were successfully sent
+
+    Step 1: Load users
+    Step 2: Check users onlinne
+    Step 3: Send introduction for whom are oline
+    Step 4: Render those which the intro was sucessfully sentt
+  
   */
 
   return (
