fccdcdeda18e54820c41101b11c008bdf229bc13
Toledodev <rafael.toledo@engenharia.ufjf.br>
2022-04-14 12:42:48 -0300

    Test done!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+  const [renderUser, setRenderUser] = useState([]);
   
   const fetchUserIds = async () => {
     return ["john.smith", "sara.lee", "jack.ma"];
@@ -29,15 +30,55 @@ const App = () => {
   
   */
 
+  async function fetchUser() {
+      // const usersArray = [];
+      const users = await fetchUserIds();
+      console.log(users);
+      return users;
+  }
+
+  async function checkStatusUser () {
+    const users = await fetchUser();
+    const statusUser = [];
+     for (let i = 0; i < users.length; i++) {
+      statusUser.push(await checkStatus(users[i]));
+    }
+    console.log(statusUser)
+      return statusUser;
+  }
+
+  async function sendEmailUser () {
+    const users = await fetchUser();
+    const statusUser = await checkStatusUser();
+    const usersArray = [];
+    for (let i = 0; i < users.length; i++) { 
+    if (statusUser[i].status === 'online') {
+      const sendEmailUser = await sendEmail(users[i])
+      console.log(sendEmailUser);
+      usersArray.push(statusUser);
+    }}
+    console.log(usersArray);
+    setRenderUser(usersArray);
+  }
+  
+useEffect(() => {
+  sendEmailUser();
+}, []); 
+console.log(renderUser, 'x')
+
   return (
     <div className="App">
       <div className="App-header">
         <div>
           All online users that introductions were sucessfully sent
           <ul>
-            <li>Student 1</li>
-            <li>Student 2</li>
-            <li>Student 3</li>
+          { renderUser.map((user, index) => 
+          <li 
+          key={index}
+          >
+            {user[index].id}
+          </li>
+          )}
           </ul>
         </div>
       </div>
