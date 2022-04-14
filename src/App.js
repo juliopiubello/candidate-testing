bb5f5e63c7c920c49e2915b3d8b2f2d62ab73133
MuriloAmaral <muralha2000@gmail.com>
2022-04-14 13:11:58 -0300

    D. one git status!

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+
+  const [renderUser, setRenderUser] = useState([]);
   
   const fetchUserIds = async () => {
     return ["john.smith", "sara.lee", "jack.ma"];
@@ -18,6 +20,28 @@ const App = () => {
     return Math.random() > 0.1 ? true : false;
   };
 
+  const exercicio = async () => {
+    const usersArray = [];
+    const users = await fetchUserIds()
+    console.log(users);
+    for (let i = 0; i < users.length ; i ++){
+      const user = await checkStatus(users[i]);
+      console.log(user);
+      if (user.status === 'online') {
+        const sendEmailUser = await sendEmail(users[i])
+        console.log(sendEmailUser);
+        usersArray.push(user)
+    }
+    console.log(usersArray);
+  }
+  setRenderUser(usersArray);
+}
+
+  useEffect(() => {
+    exercicio();
+    }, []);
+    
+  
   /*
     Question 1: 
     Find all online users and send them emails. Render the users for which the emails were successfully sent
@@ -33,11 +57,11 @@ const App = () => {
     <div className="App">
       <div className="App-header">
         <div>
-          All online users that introductions were sucessfully sent
           <ul>
-            <li>Student 1</li>
-            <li>Student 2</li>
-            <li>Student 3</li>
+          All online users that introductions were sucessfully sent
+          {renderUser.map((user, index) => 
+              <li key={index}>{user.id}</li>
+            )}
           </ul>
         </div>
       </div>
